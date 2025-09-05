9665cc46f5b94cf9335ccffa43bbfc4e30364d97
Mateo <m4teo.31dlcr@gmail.com>
2025-09-05 08:19:32 -0500

    subiendo ejer1 de la semana1

---
+++ Desktop/semana3vier/fa_semana1vier/semana1-c#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;
+            Console.Write("ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+            
+        } 
+        
+    }           
+}
